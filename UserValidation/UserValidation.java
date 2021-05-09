ecc2ca00836fd77d90565e4e12d702dceefc61e4
veeresh97 <67183089+veeresh97@users.noreply.github.com>
2021-05-09 17:16:08 +0530

    Added: code to verify valid last name

--- UserValidation/UserValidation.java
+++ UserValidation/UserValidation.java
@@ -5,7 +5,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserValidation {
-public static boolean validFirstName(String name) {
+public static boolean validName(String name) {
         String regex = "^[A-Z][a-zA-Z]{2,}";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(name);
@@ -16,10 +16,15 @@ public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter firstName : ");
         String firstName = scan.nextLine();
-        System.out.println("First name :"+validFirstName(firstName));
-        if(validFirstName(firstName))
-        System.out.println("name is valid");
+        System.out.println("First name :"+validName(firstName));
+        System.out.println("Enter last  Name: ");
+        String lastName = scan.nextLine();
+        System.out.println("Last name :"+validName(lastName));
+        if(validName(firstName))
+        System.out.println("Firstname is valid");
+        if(validName(lastName))
+        System.out.println("Lastname is valid");
         else
-        System.out.println("name is invalid");
+                System.out.println("Invalid name");
     }
 }
