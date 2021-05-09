867a97b876977d75866f3f0a816fb900fca7aece
veeresh97 <67183089+veeresh97@users.noreply.github.com>
2021-05-09 19:09:17 +0530

    Added: Password is to be entered having 8 character minimum

--- UserValidation/UserValidation.java
+++ UserValidation/UserValidation.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class UserValidation {
 static Scanner sc = new Scanner(System.in);
-        public static void validation(String firstName, String lastName, String email, String mobile) {
+        public static void validation(String firstName, String lastName, String email, String mobile, String password ) {
                 // First Name and Last Name Validation
                 System.out.println("First Name: " + Pattern.matches("^[A-Z]{1}[a-zA-Z]{2,}$", firstName));
                 System.out.println("Last Name: " + Pattern.matches("^[A-Z]{1}[a-zA-Z]{2,}$", lastName));
@@ -13,6 +13,8 @@ static Scanner sc = new Scanner(System.in);
                 System.out.println("E-Mail: " + Pattern.matches("[a-zA-Z-\\+]+(\\.[a-zA-Z0-9-]+)*@[A-Za-z-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$", email));
                 // PhoneNumber validation
                 System.out.println("Mobile Number: " + Pattern.matches("^[9][1][\s][6-9]{1}[0-9]{9}$", mobile));
+                // Password validation (Rule 1: minimum 8 characters to be there)
+                System.out.println("Password: " + Pattern.matches("^[a-zA-Z0-9]{8,}$", password));
         }
         public static void main(String[] args) {
                 System.out.println("Enter a valid first name");
@@ -23,7 +25,9 @@ static Scanner sc = new Scanner(System.in);
                 String email = sc.nextLine();
                 System.out.println("Enter a valid mobile number starting with 91 followed by a space and 10 digit number");
                 String mobile = sc.nextLine();
-                validation(firstName, lastName, email, mobile);
+                System.out.println("Enter the password and it should be minimum of 8 characters");
+                String password = sc.nextLine();
+                validation(firstName, lastName, email, mobile, password);
 
         }
 }
