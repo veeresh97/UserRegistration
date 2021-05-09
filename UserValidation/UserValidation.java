3227337824c8f5c23caf6a6c81e9bdd2b2bcf0e8
veeresh97 <67183089+veeresh97@users.noreply.github.com>
2021-05-09 17:01:54 +0530

    Added: code to verify valid 1st name

--- UserValidation/UserValidation.java
+++ UserValidation/UserValidation.java
@@ -1,7 +1,25 @@
 package UserValidation;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserValidation {
-    public static void main(String[] args) {
-        System.out.println("Welcome to User registration");
+public static boolean validFirstName(String name) {
+        String regex = "^[A-Z][a-zA-Z]{2,}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(name);
+        return matcher.matches();
+    }
+
+public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter firstName : ");
+        String firstName = scan.nextLine();
+        System.out.println("First name :"+validFirstName(firstName));
+        if(validFirstName(firstName))
+        System.out.println("name is valid");
+        else
+        System.out.println("name is invalid");
     }
 }
